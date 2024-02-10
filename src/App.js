18bc041ad93a8c50f6f6e252fbba8a4d411d607e
Shamiltk01 <shamiltk02@gmail.com>
2024-02-10 11:36:18 +0530

    register done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Register from './components/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
-      <Register/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
